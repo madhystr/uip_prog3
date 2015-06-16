e1b44726164bf9c7f9db7e5884fc89299177ca41
madhystr <made.esquivel@gmail.com>
2015-06-15 23:40:41 -0500

    Update quiz5.py

--- laboratorios/sem5/quiz5.py
+++ laboratorios/sem5/quiz5.py
@@ -1,12 +1,14 @@
 l= "LISTA DE SUPERMERCADO"
 lista=["pan","leche","azucar","sal","arroz","huevo","cereal","javon","poroto","lenteja"]
-print (l)
-print (lista)
-a = input("¿Desea agregar un articulo a la lista?" )
-lista.append(input("Inserte el nombre del articulo: "))
-print (l)
-print (lista)
-b = input("¿Desea borrar un articulo de la lista?")
-del lista[int(input("insete la posicion en que se encuentra el articulo: "))]
-print (l)
-print (lista)
+print (l, lista)
+print ("¿Que desea hacer?")
+a= int (input("1 = agregar articulo ó  2 = borrar articulo"))
+if a==1:
+	for i in range(1):
+		arti= input("inserte nombre de articulo que desea agregar")
+		lista.append(arti)
+	print (l, lista)
+else: 
+	print ("¿que articulo deseas eliminar?")
+	del lista[int(input("indica la posicion de articulo a eliminar"))]
+print (l, lista)
